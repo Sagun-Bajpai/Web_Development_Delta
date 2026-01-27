c3d24533271b9c8db64206b648297a3308f54cf8
sagun bajpai <sagunbajpaisagun@gmail.com>
2026-01-27 12:39:36 +0530

    add function file

---
+++ JAVA_Script/Day6Function/PracticeQ1.js
@@ -0,0 +1,7 @@
+function printPoem(){
+    console.log("Twincal Twincal little star.");
+    console.log("how i wonder what you are.");
+     console.log("up above the world so high");
+    console.log("like a diamond in the sky.");
+}
+printPoem();
---
+++ JAVA_Script/Day6Function/function.js
@@ -0,0 +1,27 @@
+function hello(){
+    console.log("i am sagun");
+}
+hello();
+
+function printAge(){
+    console.log("21");
+}
+printAge();
+
+function isAdult(){
+    let age=13;
+    if(age>=18){
+        console.log("adult");
+    }
+    else{
+        console.log("not adult");
+    }
+}
+isAdult();
+
+function print1_6(){
+    for(let i=0;i<=6;i++){
+        console.log(i);
+    }
+}
+print1_6();
