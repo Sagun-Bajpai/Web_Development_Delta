da793717caea0fd4dc31aa1e8ea7b6aa4fa95262
sagun bajpai <sagunbajpaisagun@gmail.com>
2026-03-07 11:30:49 +0530

    calllback hell

---
+++ JAVA_Script/Day11CallStack/callbackHell.js
@@ -0,0 +1,17 @@
+H1=document.querySelector("H1");
+function changeColor(color,delay,nextColorChange){
+    setTimeout(()=>{
+        H1.style.color=color;
+        if(nextColorChange) nextColorChange();
+    },delay);
+}
+
+changeColor("red",1000,() =>{
+    changeColor("pink",1000,()=>{
+        changeColor("yellow",1000,()=>{
+             changeColor("blue",1000,()=>{
+                 changeColor("purple",1000);
+             });
+        });
+    });
+});
